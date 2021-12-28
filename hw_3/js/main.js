fa54ea388bba116bd69778a50385afe65bd60367
Юля <jula@MacBook-Pro-admin.local>
2021-12-28 18:05:31 +0200

    add new functions

--- hw_3/js/main.js
+++ hw_3/js/main.js
@@ -1,18 +1,43 @@
-const myInput = document.getElementById("input-area");
-const result =document.getElementById("result");
-const error = document.getElementById("error");
-function maxDigit(n){
-if(n==0){
-    return 0;
-}else{
-    return Math.max(n%10, maxDigit(n/10));
-    
-}
-}
-const ceil=Math.floor(maxDigit(4694));
-console.log(ceil);
+
+// const n = prompt('please enter your number')
+// function maxDigit(n){
+// if(n==0){
+//     return 0;
+//  }else{
+//     return Math.max(n%10, maxDigit(n/10));
+
+//  }
+// }
+// const ceil=Math.floor(maxDigit(n));
+//  console.log(ceil);
 
 
+// let str = prompt ("enter your name");
+// function capitalizeFirstLetter(str){
+//     return str[0].toUpperCase()+str.substring(1).toLowerCase();
+// }
+// // console.log(capitalizeFirstLetter('vItaLIk') );
+// console.log(capitalizeFirstLetter(str));
 
+// const salary = prompt('enter your salary');
+// const tax = prompt('enter your tax');
+// // var calcPercent = (salary, tax) => salary / 100 * tax;
+// // calcPercent(salary, tax);
+// // result = salary - calcPercent;
+// // console.log(result);
+// calcPercent = salary/ 100 * tax ;
+// result = salary - calcPercent;
+// console.log(result);
 
+// let min = 10;
+// let max = 20;
+// function getRandomNumber(min, max){
+//     return Math.round(Math.random() * (max - min)+ min);
+// }console.log(getRandomNumber(min, max));
+
+// let string = prompt("enter rhe word,that we count how many letter 'a' used in it ");
+// let letter = 'a';
+// var count = string.split("a").length - 1;
+// console.log(count)
+ 
 
