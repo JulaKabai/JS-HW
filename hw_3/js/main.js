2af86c13253df55cc983de2021c9a111973eeb95
Kabai <uliakabai0@gmail.com>
2022-01-15 14:31:06 +0200

    correct my tasks

--- hw_3/js/main.js
+++ hw_3/js/main.js
@@ -1,41 +1,85 @@
+const biggestNumberForm = document.getElementById("biggest-number-form");
+const biggestNumberResult = document.getElementById("biggest-number-result");
+const biggestNumberError = document.getElementById("biggest-number-error");
 
 
-
-
-const n = prompt("please enter your number");
-function maxDigit(n) {
-	if (n == 0) {
-		return 0;
+biggestNumberForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const number = event.target[0].value;
+	biggestNumberResult.innerHTML = "";
+	biggestNumberError.innerHTML = ""
+	if (+number >= 0) {
+		biggestNumberResult.innerHTML = maxDigit(number);
 	} else {
-		return Math.max(n % 10, maxDigit(n / 10));
+		biggestNumberError.innerHTML = "The number must be greater than null"
 	}
-}
-const ceil = Math.floor(maxDigit(n));
-console.log(ceil);
+})
+
+const maxDigit = (number) => Math.max(...number.split(""));
+
 
 ////////////////////////////////////////////
-// let str = prompt ("enter your name");
-// function capitalizeFirstLetter(str){
-//     return str[0].toUpperCase()+str.substring(1).toLowerCase();
-// }
-// // console.log(capitalizeFirstLetter('vItaLIk') );
-// console.log(capitalizeFirstLetter(str));
+
+
+const firstLetterCapitalizeForm = document.getElementById("first-letter-capitalize-form");
+const firstLetterCapitalizeResult = document.getElementById("first-letter-capitalize-result");
+
+firstLetterCapitalizeForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const name = event.target[0].value;
+	firstLetterCapitalizeResult.innerHTML = capitalizeFirstLetter(name);
+	
+}) 
+
+const capitalizeFirstLetter = str => str[0].toUpperCase()+str.substring(1).toLowerCase();
+
+
 ///////////////////////////////////////////
-// const salary = prompt('enter your salary');
-// const tax = prompt('enter your tax');
-// // var calcPercent = (salary, tax) => salary / 100 * tax;
-// // calcPercent(salary, tax);
-// // result = salary - calcPercent;
-// // console.log(result);
-// calcPercent = salary/ 100 * tax ;
-// result = salary - calcPercent;
-// console.log(result);
+
+
+const calculatingSalaryDifferenceForm = document.getElementById("calculating-salary-difference-form");
+const calculatingSalaryDifferenceResult = document.getElementById("calculating-salary-difference-result");
+const calculatingSalaryDifferenceError = document.getElementById("calculating-salary-difference-error");
+
+calculatingSalaryDifferenceForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+	const salary = event.target[0].value;
+	const tax = event.target[1].value;
+	calculatingSalaryDifferenceResult.innerHTML = '';
+	calculatingSalaryDifferenceError.innerHTML = '';
+	if (salary > 0 && tax > 0) {
+	  calculatingSalaryDifferenceResult.innerHTML = calcPercent(salary, tax);
+  } else{ 
+	  calculatingSalaryDifferenceError.innerHTML = "The number must be greater than null";
+  }
+})
+
+// true && true => true
+// true && false => false
+// false && true => false
+// false && false => false
+
+const calcPercent = (salary , tax) => salary - (salary / 100 * tax);
+
 /////////////////////////////////////////////
-// let min = 10;
-// let max = 20;
-// function getRandomNumber(min, max){
-//     return Math.round(Math.random() * (max - min)+ min);
-// }console.log(getRandomNumber(min, max));
+const returnFirstRandomNumberForm = document.getElementById('return-random-number-form');
+const returnRandomNumberResult = document.getElementById('return-random-number-result');
+const ReturnRandomNumberError = document.getElementById('return-random-number-error');
+returnFirstRandomNumberForm .addEventListener("submit", (event) => {
+  event.preventDefault();
+  const min = +event.target[0].value;
+	const max = +event.target[1].value;
+	returnRandomNumberResult.innerHTML = ' ';
+	ReturnRandomNumberError.innerHTML = ' ';
+	if(min < max) {
+	  returnRandomNumberResult.innerHTML = getRandomNumber(min, max);
+	}else{
+		ReturnRandomNumberError.innerHTML = "the first number mast to be less then second"
+	}
+}) 
+
+const getRandomNumber = (min, max) =>  Math.round(Math.random() * (max - min)+ min);
+
 //////////////////////////////////////////////
 // let string = prompt("enter rhe word,that we count how many letter 'a' used in it ");
 // let letter = 'a';
