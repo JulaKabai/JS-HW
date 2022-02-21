b8e88a49ded6b70b2792b396235738ce53a175cf
Kabai <uliakabai0@gmail.com>
2022-02-21 17:44:57 +0200

    add new tasks

--- hw_8/js/hw.js
+++ hw_8/js/hw.js
@@ -1,33 +1,65 @@
 class Student {
-    constructor(university, course, fullName, marks){
-this.university = university;
-this.course = course;
-this.fullName = fullName;
-this.marks = marks;
-    }
-    getInfo(){
-        return this.course + ' ' + this.university + ' , ' + this.fullName
-    }
-    get studentsMark(){
-        return this.marks
-    }
-    set studentsMark(newMark){
-        this.marks.push(newMark)
-    }
-    getMidNote(){
-        const midNote = this.marks.flat().reduce((sum, current) =>  sum + current);
-        return midNote / this.marks.flat().length
-    }
+	constructor(university, course, fullName, marks) {
+		this.university = university;
+		this.course = course;
+		this.fullName = fullName;
+		this.marks = marks;
+	}
+	getInfo() {
+		return this.course + " " + this.university + " , " + this.fullName;
+	}
+	get studentsMark() {
+		return this.marks;
+	}
+	set studentsMark(newMark) {
+		this.marks.push(newMark);
+	}
+	getMidNote() {
+		const midNote = this.marks.flat().reduce((sum, current) => sum + current);
+		return midNote / this.marks.flat().length;
+	}
 }
 
+class BudgetStudent extends Student{
+	constructor(scholarship, university, course, fullName, marks){
+	super(university, course, fullName, marks)
+	this.scholarship = scholarship;
+	}
+	get getScholarship() {
+		if(student3.getMidNote() <= 5) {
+				return setInterval(() => console.log('Ви отримали' + ' ' + this.scholarship + 'uah' + ' ' + 'стипендії'),3000)
+			}else{
+				return this.studentsMark
+			}
+		}
+}
+const student = new Student(
+	"ЛНУ",
+	"3 курс",
+	"Бенько Ярина Тарасівна",
+	[5, 4, 4, 5]
+);
+const student2 = new Student(
+	"Львівська Політехніка",
+	"1 курс",
+	"Требко Анна Дмитрівна",
+	[5, 5, 4, 5]
+);
+const student3 = new Student(
+	"УАД",
+	"2 курс",
+	"Гірченко Іван Іванович",
+	[2, 4, 4, 3]
+);
 
-const student = new Student('ЛНУ', '3 курс', 'Бенько Ярина Тарасівна',[5, 4, 4, 5])
-const student2 = new Student('Львівська Політехніка', '1 курс', 'Требко Анна Дмитрівна', [5, 5, 4, 5])
-const student3 = new Student('УАД', '2 курс', 'Гірченко Іван Іванович', [2, 4, 4, 3])
-console.log(student2.getInfo())
-console.log(student3.getInfo())
-//console.log('student:', student2)
-console.log(student.studentsMark)
+console.log(student2.getInfo());
+console.log(student3.getInfo());
+console.log('student:', student2)
+console.log(student.studentsMark);
 student.studentsMark = 5;
-console.log(student.studentsMark)
-console.log(student.getMidNote())
+console.log(student.studentsMark);
+console.log(student.getMidNote());
+const payment = new BudgetStudent(1400,student3)
+//payment.getScholarship
+
+
