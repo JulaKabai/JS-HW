d0f5b5120a8d7b27a6c08cabfaa1551bcef323a6
Kabai <uliakabai0@gmail.com>
2022-07-09 03:31:33 +0300

    finish my hw

--- hw_14/src/index.js
+++ hw_14/src/index.js
@@ -1,2 +1,5 @@
 import hw_1 from '../../hw_1/js/main';
 import hw_2 from '../../hw_4/js/main';
+import hw_3 from '../../hw_5/hw';
+import hw_4 from '../../hw_7/js/hw'
+import hw_5 from '../../hw_3/js/main'
